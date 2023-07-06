0078442e909182358bff7bccc4dfb0411a9e0317
jsingvah <79827219+jsingvah@users.noreply.github.com>
2023-07-06 13:42:49 -0600

    Nim Game Menu

--- Nim-WaterfallMVC/Controllers/HomeController.cs
+++ Nim-WaterfallMVC/Controllers/HomeController.cs
@@ -18,6 +18,34 @@ namespace Nim_WaterfallMVC.Controllers
             return View();
         }
 
+        public IActionResult PVPInput()
+        {
+            return View();
+        }
+
+        public IActionResult PVCInput()
+        {
+            return View();
+        }
+
+        public IActionResult Rule()
+        {
+            return View();
+        }
+
+        public IActionResult Game(string P1Name, string P2Name)
+        {
+            if (P2Name == null)
+            {
+                P2Name = "Computer";
+            }
+
+            ViewBag.P1Name = P1Name;
+            ViewBag.P2Name = P2Name;
+
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
