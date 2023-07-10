3a4ff5d709adbe28ed6180bcb90b3db52599bc91
wmcentire <wmcentire@student.neumont.edu>
2023-07-10 14:34:08 -0600

    7/10/23 WAM added comments to board object

--- Nim-WaterfallMVC/Models/Board.cs
+++ Nim-WaterfallMVC/Models/Board.cs
@@ -24,14 +24,16 @@
             dif = difficulty;
             StartGame();
         }
-
+        /// <summary>
+        /// creates and populates the rows, and sets first player
+        /// </summary>
         public void StartGame()
         {
             rows = new Rows[4+dif];
-            for(int j = 0; j < rows.Length;j++)
+            for(int j = 0; j < rows.Length;j++) // create a populate rows
             {
-                int length = 1 + 2 * j;
-                rows[j] = new Rows(length);
+                int length = 1 + (2 * j);
+                rows[j] = new Rows(length); // creates new row with length 2 greater than the last
                 for(int i = 0; i < rows[j].len; i++)
                 {
                     rows[j].matches[i] = true;
@@ -39,14 +41,20 @@
             }
             currentPlayer = players[turn];
         }
-
+        /// <summary>
+        /// calls updateRows, then checks to see if there are any matches left. 
+        /// Returns true if no matches are left, and false if there are still matches
+        /// </summary>
+        /// <param name="rowIdx"></param>
+        /// <param name="matchesRemoved"></param>
+        /// <returns></returns>
         public bool PassTurn(int rowIdx, int matchesRemoved)
         {
             updateRows(rowIdx, matchesRemoved);
             turn++;
-            turn = turn % 2;
-            currentPlayer = players[turn];
-            bool hasMatches = false;
+            turn = turn % 2; // sets the turn to either 0 or 1
+            currentPlayer = players[turn]; // updates current player
+            bool hasMatches = false; // the loop below checks to see if there are any matches left
             foreach(Rows row in rows)
             {
                 if (!hasMatches)
@@ -58,13 +66,17 @@
                     break;
                 }
             }
-            if (!hasMatches)
+            if (!hasMatches) // if there are no matches left, return true
             {
                 return true;
-            }
+            } // else return false
             return false;
         }
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="rowIdx"></param>
+        /// <param name="matchesRemoved"></param>
         public void updateRows(int rowIdx, int matchesRemoved)
         {
             for(int i = rows[rowIdx].len - rows[rowIdx].matchCount(); i < rows[rowIdx].len - rows[rowIdx].matchCount() + matchesRemoved; i++) // len - matches lit // 5 - 3 = 2
