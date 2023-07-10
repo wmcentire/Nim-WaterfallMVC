f1dde103b653a563c602b2a188cca56fa27de891
jsingvah <79827219+jsingvah@users.noreply.github.com>
2023-07-10 14:06:34 -0600

    Radio Buttons (Difficulty)

--- Nim-WaterfallMVC/wwwroot/js/site.js
+++ Nim-WaterfallMVC/wwwroot/js/site.js
@@ -2,3 +2,20 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//const btnPVP = document.getElementById('btnPVP');
+//const btnTextPVP = document.getElementById('btnTextPVP');
+
+//btnPVP.addEventListener('mouseover', btnChange);
+//btnPVP.addEventListener('mouseout', btnReverse);
+//btnTextPVP.addEventListener('mouseover', btnChange);
+
+//function btnChange() {
+//    btnPVP.src = '~/images/Nim-ButtonHighlighted.png';
+//    btnTextPVP.style.color = 'black';
+//}
+
+//function btnReverse()
+//{
+//    btnPVP.src = '~/images/Nim-Button.png';
+//}
